0424cd12ca76465af063e2f582bce08b54e8a1bd
zwelinux <zwelinux@gmail.com>
2024-03-20 09:10:41 +0630

    20_3_2024

---
+++ testing16.py
@@ -0,0 +1,17 @@
+# num1 = 16
+# num1 += 1
+# print(num1)
+
+# x = ["1", "2", "3"]
+# y = ["1", "2", "3"]
+# z = x
+
+# print(x is y)
+# print(x is z)
+
+developergp = ["zwe", "ahkar", "htet"]
+name = input("Enter your name : ")
+if name in developergp:
+    print("Hello, Developer")
+else:
+    print("Hello Guest")
